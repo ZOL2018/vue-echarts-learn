9b28c75182c558bfc06db9195c295631acbd54bb
zhoulibin <libin.zhou@ecloud.com.cn>
2018-11-23 15:28:01 +0800

    vuex 配置 显示安装

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.readMe with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuex from 'vuex'
 import router from './router'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
